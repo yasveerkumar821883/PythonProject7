2181593f8c1c12cb89c09a7de8f8fc998d8c84a8
yasve <yasveerkumar821883@gmail.com>
2026-02-11 14:28:27 +0530

    list all operation

---
+++ operations.py
@@ -0,0 +1,14 @@
+l1=["yash",243,"India","Ayodhya"]
+l1.append("RAM")#add element end off the list
+l2=[24,53]
+#l1.extend(l2)#add list end of list
+#l1.insert(2,"rohan")
+new_l1=l1.copy()
+ind=l1.index("yash")
+print("return append list : ",l1)
+print("return extend list : ",l1)
+print("return inserted list : ",l1)
+print("print the copy of list 1 : ",new_l1)
+print('print the index of \"Yash\"',ind)
+
+
